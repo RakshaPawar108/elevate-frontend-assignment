a48981f3fc8cc18aa1d85c13fb47f084ef50540c
Raksha Pawar <rakshapawar10.sae@gmail.com>
2023-02-03 20:28:36 +0530

    style: add styling to banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,10 +1,15 @@
+import { bannerImg } from "../../assets";
 import "./Banner.css";
 
 export const Banner = () => {
   return (
     <div className="banner-wrapper">
       <div className="banner-background-image-wrapper">
-        <div className="banner-background-image overlay">
+        <img
+          src={bannerImg}
+          className="banner-background-image"
+          alt="Banner Image"
+        />
           <div className="overlay-text-wrapper">
             <h3 className="banner-heading">Get Started</h3>
             <h3 className="banner-heading">Your Favourite Shopping Site</h3>
@@ -12,7 +17,6 @@ export const Banner = () => {
               <button className="intro-btn">Buy Now</button>
             </a>
           </div>
-        </div>
       </div>
     </div>
   );
