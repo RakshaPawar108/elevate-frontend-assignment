68d2157532576c6a5e0c10401145f2593a0737be
Raksha Pawar <rakshapawar10.sae@gmail.com>
2023-02-03 21:44:11 +0530

    style: add styling to Products Section

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,5 +1,9 @@
+import "./Products.css";
+
 export const Products = () => {
-    return <div className="products-wrapper">
-        <div className="products-category-heading">Men's & Women's Fashion</div>
+  return (
+    <div className="products-wrapper">
+      <div className="products-category-heading">Men's & Women's Fashion</div>
     </div>
-}
+  );
+};
