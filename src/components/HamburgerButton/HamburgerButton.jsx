df915e48152933bd13cd1c7722873e5ce654a27f
Raksha Pawar <rakshapawar10.sae@gmail.com>
2023-02-04 11:37:07 +0530

    feat: add HamburgerButton component

---
+++ src/components/HamburgerButton/HamburgerButton.jsx
@@ -0,0 +1,10 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
+export const HamburgerButton = () => {
+  return (
+    <div>
+      <FontAwesomeIcon icon={faBars} />
+    </div>
+  );
+};
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export { Banner } from "./Banner/Banner";
 export { Products } from "./Products/Products";
 export { Product } from "./Product/Product";
+export { HamburgerButton } from "./HamburgerButton/HamburgerButton";
+export { FunctionBar } from "./FunctionBar/FunctionBar";
