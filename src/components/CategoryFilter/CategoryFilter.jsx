61eb61c52d6bec64272487dfd3c888216dcb7928
Raksha Pawar <rakshapawar10.sae@gmail.com>
2023-02-04 15:40:09 +0530

    style: add styling to category filter

--- src/components/CategoryFilter/CategoryFilter.jsx
+++ src/components/CategoryFilter/CategoryFilter.jsx
@@ -1,17 +1,29 @@
+import "./CategoryFilter.css";
+
 export const CategoryFilter = () => {
   return (
     <div className="category-filter-wrapper">
-      <select>
-        <option value="All">All Categories</option>
+      <select className="filter-options">
+        <option className="filter-option" value="All">
+          All Categories
+        </option>
         {/* {categories.map((category) => (
           <option key={category} value={category}>
             {category}
           </option>
          ))} */}
-        <option value="men">Men's Clothing</option>
-        <option value="women">Women's Clothing</option>
-        <option value="electronics">Electronics</option>
-        <option value="jewellery">Jewellery</option>
+        <option className="filter-option" value="men">
+          Men's Clothing
+        </option>
+        <option className="filter-option" value="women">
+          Women's Clothing
+        </option>
+        <option className="filter-option" value="electronics">
+          Electronics
+        </option>
+        <option className="filter-option" value="jewellery">
+          Jewellery
+        </option>
       </select>
     </div>
   );
