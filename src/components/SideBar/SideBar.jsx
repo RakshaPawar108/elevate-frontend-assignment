e4720e374aebd83bea6d9144f5b08e6285e5c188
Raksha Pawar <rakshapawar10.sae@gmail.com>
2023-02-04 12:23:34 +0530

    style: add styling to sidebar

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,3 +1,33 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import "./SideBar.css";
+
 export const SideBar = () => {
-  return <aside className="sidebar">Sidebar</aside>;
+  return (
+    <aside className="sidebar">
+      <FontAwesomeIcon className="cross-icon" icon={faXmark} />
+      <ul className="sidebar-list-items">
+        <li className="list-item">
+          <a href="#home" className="list-item-link">
+            Home
+          </a>
+        </li>
+        <li className="list-item">
+          <a href="#fashion" className="list-item-link">
+            Fashion
+          </a>
+        </li>
+        <li className="list-item">
+          <a href="#electronics" className="list-item-link">
+            Electronics
+          </a>
+        </li>
+        <li className="list-item">
+          <a href="#jewellery" className="list-item-link">
+            Jewellery & Accessories
+          </a>
+        </li>
+      </ul>
+    </aside>
+  );
 };
